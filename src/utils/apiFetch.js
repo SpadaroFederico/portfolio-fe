32369819705c1751097b6b3df6a0132bc16f7401
Federico Spadaro <federicospadaro2@gmail.com>
2025-08-28 18:52:13 +0200

    fix: Update apiFetch to handle 401 status and improve comments for clarity

--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,4 +1,6 @@
 // utils/apiFetch.js
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 export const apiFetch = async (url, options = {}) => {
   const res = await fetch(`${BASE_URL}${url}`, { ...options, credentials: 'include' });
 
@@ -11,7 +13,7 @@ export const apiFetch = async (url, options = {}) => {
     if (refresh.ok) {
       return await fetch(`${BASE_URL}${url}`, { ...options, credentials: 'include' });
     } else {
-      return { status: 401 }; // non redirect
+      return { status: 401 }; // lascia gestire ProtectedRoute
     }
   }
 
